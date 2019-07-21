9f8a081a4534b8002316a56bee5aa50538c1b568
Dmitry Shlensky <shld@list.ru>
2019-07-21 15:26:50 +0300

    add theme switch logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
 require('./style.scss');
+
+(function () {
+    document.body.addEventListener('click', function (e) {
+        if (e.target.classList.contains('onoffswitch') || e.target.classList.contains('onoffswitch__button')) {
+            let themedItems = document.querySelectorAll('.theme_color_project-default, .theme_color_project-inverse');
+            themedItems.forEach((themedItem) => {
+                if (themedItem.classList.contains('theme_color_project-default')) {
+                    themedItem.classList.remove('theme_color_project-default');
+                    themedItem.classList.add('theme_color_project-inverse');
+                } else {
+                    themedItem.classList.remove('theme_color_project-inverse');
+                    themedItem.classList.add('theme_color_project-default');
+                }
+            });
+        }
+    })
+})();
